2c6bf489de23795ea02a287207080a844a4b9b73
githubfoam <lequestionis@gmail.com>
2018-12-16 23:42:29 +0300

    correction added

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,12 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+apt_update 'Update the apt cache daily' do
+  frequency 86_400
+  action :periodic
+end
+
+package 'auditd' do
+  action :install
+end
